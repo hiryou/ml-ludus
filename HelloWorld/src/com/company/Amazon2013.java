38317cd0fa34c62e912bdd69b9fa44c13613551a
Long Nguyen <longn@zillowgroup.com>
2022-05-21 16:38:14 -0400

    Restructure directories; Added personal notes

--- HelloWorld/src/com/company/Amazon2013.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * What I remember from my phone screen & on-site interview with Amazon back in 2013
- */
-public class Amazon2013 {
-}
--- HelloWorld/src/com/company/Google2013.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * What I remember from my phone screen & on-site interview with Google back in August 2013
- */
-public class Google2013 {
-}
