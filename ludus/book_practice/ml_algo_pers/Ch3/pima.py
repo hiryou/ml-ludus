c390702ee15f4e1b900ad83c0d4d1f27e521990d
Long Nguyen <longn@zillowgroup.com>
2022-06-09 11:39:23 -0400

    working......

--- ludus/book_practice/ml_algo_pers/Ch3/pima.py
+++ ludus/book_practice/ml_algo_pers/Ch3/pima.py
@@ -11,29 +11,30 @@
 # Demonstration of the Perceptron on the Pima Indian dataset
 # This dataset is now on kaggle https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database
 
+# Just press F5 - runs interactively in IDE
+
 import pylab as pl
+import os
 import numpy as np
-#import pcn
+import pcn
 
-pima = np.loadtxt('./diabetes.csv', delimiter=',')
+included_header = np.genfromtxt(os.getcwd() + '/diabetes.csv', delimiter=',')
+pima = included_header[1:]
 
-print(pima)
-
-"""
-
-# Plot the first and second values for the two classes
+# filtered for datapoints belong to 1st class
 indices0 = np.where(pima[:,8]==0)
+# filtered for datapoints belong to 2nd class
 indices1 = np.where(pima[:,8]==1)
-
-pl.ion()
-pl.plot(pima[indices0,0],pima[indices0,1],'go')
-pl.plot(pima[indices1,0],pima[indices1,1],'rx')
+# Plot the feature 1 and 2 for each class
+#pl.ion()
+pl.plot(pima[indices0,0], pima[indices0,1], 'go')
+pl.plot(pima[indices1,0], pima[indices1,1], 'rx')
 
 # Perceptron training on the original dataset
 print("Output on original data")
-p = pcn.pcn(pima[:,:8],pima[:,8:9])
-p.pcntrain(pima[:,:8],pima[:,8:9],0.25,100)
-p.confmat(pima[:,:8],pima[:,8:9])
+p = pcn.pcn(pima[:,:8], pima[:,8:9])
+p.pcntrain(pima[:,:8], pima[:,8:9], 0.25, 100)
+p.confmat(pima[:,:8], pima[:,8:9])
 
 # Various preprocessing steps
 pima[np.where(pima[:,0]>8),0] = 8
@@ -64,6 +65,4 @@ p1.pcntrain(trainin,traintgt,0.25,100)
 p1.confmat(testin,testtgt)
 
 
-
-pl.show()
-"""
+#pl.show()
